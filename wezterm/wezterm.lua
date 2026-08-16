de77042e8ff6650d456f13a88b7e40413a90c653
Abdul Rafay <abdulrafayaslam.007@gmail.com>
2026-08-16 17:41:04 +0500

    Remove quit confirmation for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,6 +9,7 @@ config.macos_window_background_blur = 50
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE|MACOS_FORCE_ENABLE_SHADOW"
 config.use_fancy_tab_bar = false
+config.window_close_confirmation = "NeverPrompt"
 config.colors = {
 	tab_bar = {
 		background = "#191724",
